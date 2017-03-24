4a9e57358b3d70e27f06298a12c1d48a63be73fd
mdriscoll <mdriscoll@T3610.agleader.local>
2017-03-24 08:47:21 -0500

    added new examples and updated another

--- examples/draw_lines.py
+++ examples/draw_lines.py
@@ -2,26 +2,26 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
 
-def draw_strings(my_canvas):
-    my_canvas.drawString(30,750, 'John Doe')
-    my_canvas.drawString(30,735,'123 AnyWhere Ave')
-    my_canvas.drawString(30,720,"Menlo Park CA 12345")
+def draw_strings(canvas_obj):
+    canvas_obj.drawString(30,750, 'John Doe')
+    canvas_obj.drawString(30,735,'123 AnyWhere Ave')
+    canvas_obj.drawString(30,720,"Menlo Park CA 12345")
 
 
-def draw_lines(my_canvas):
-    my_canvas.setLineWidth(.3)
-    my_canvas.setFont('Helvetica', 12)
+def draw_lines(canvas_obj):
+    canvas_obj.setLineWidth(.3)
+    canvas_obj.setFont('Helvetica', 12)
 
     start_y = 710
-    my_canvas.line(30, start_y, 580, start_y)
+    canvas_obj.line(30, start_y, 580, start_y)
 
     for x in range(10):
         start_y -= 10
-        my_canvas.line(30, start_y, 580, start_y)
+        canvas_obj.line(30, start_y, 580, start_y)
 
 
 if __name__ == '__main__':
-    my_canvas = canvas.Canvas("lines.pdf", pagesize=letter)
-    draw_strings(my_canvas)
-    draw_lines(my_canvas)
-    my_canvas.save()
+    canvas_obj = canvas.Canvas("lines.pdf", pagesize=letter)
+    draw_strings(canvas_obj)
+    draw_lines(canvas_obj)
+    canvas_obj.save()
