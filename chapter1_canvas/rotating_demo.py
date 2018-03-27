b0d4225c1232e60f3119c1cddd85794d4dab17bb
Mike Driscoll <mike@pythonlibrary.org>
2018-03-26 20:18:42 -0500

    Updated canvas rotating demo

--- chapter1_canvas/rotating_demo.py
+++ chapter1_canvas/rotating_demo.py
@@ -4,16 +4,23 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import inch
 from reportlab.pdfgen import canvas
 
+
 def rotate_demo():
     my_canvas = canvas.Canvas("rotated.pdf",
                               pagesize=letter)
     my_canvas.translate(inch, inch)
     my_canvas.setFont('Helvetica', 14)
     my_canvas.drawString(inch, inch, 'Normal')
+    my_canvas.line(inch, inch, inch+100, inch)
+
     my_canvas.rotate(45)
     my_canvas.drawString(inch, -inch, '45 degrees')
+    my_canvas.line(inch, inch, inch+100, inch)
+
     my_canvas.rotate(45)
     my_canvas.drawString(inch, -inch, '90 degrees')
+    my_canvas.line(inch, inch, inch+100, inch)
+
     my_canvas.save()
 
 if __name__ == '__main__':
