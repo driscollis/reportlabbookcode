eb3dfdcc36ce9a0bbdc61e0467dfeb49b1ebe240
Mike Driscoll <mike@pythonlibrary.org>
2018-04-06 22:04:35 -0500

    Changed a long line

--- chapter16_pypdf/pdf_encryption.py
+++ chapter16_pypdf/pdf_encryption.py
@@ -5,14 +5,15 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 def encrypt(input_pdf, output_pdf, password):
     pdf_writer = PdfFileWriter()
     pdf_reader = PdfFileReader(input_pdf)
-    
+
     for page in range(pdf_reader.getNumPages()):
         pdf_writer.addPage(pdf_reader.getPage(page))
-        
-    pdf_writer.encrypt(user_pwd=password, owner_pwd=None, use_128bit=True)
+
+    pdf_writer.encrypt(user_pwd=password, owner_pwd=None,
+                       use_128bit=True)
     with open(output_pdf, 'wb') as fh:
         pdf_writer.write(fh)
-        
+
 if __name__ == '__main__':
     encrypt(input_pdf='reportlab-sample.pdf',
             output_pdf='encrypted.pdf',
