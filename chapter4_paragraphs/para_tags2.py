aedc65ee084bbcf64a91ea2e249030586bf86383
Mike Driscoll <mike@pythonlibrary.org>
2018-04-09 15:00:14 -0500

    Fixed out put names and added new example

--- chapter4_paragraphs/para_tags2.py
+++ chapter4_paragraphs/para_tags2.py
@@ -1,20 +1,22 @@
-# para_tags.py
+# para_tags2.py
 
+from reportlab.lib.enums import TA_CENTER
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 
 def paragraph_para_markup():
-    doc = SimpleDocTemplate("para_tags.pdf",
+    doc = SimpleDocTemplate("para_tags2.pdf",
                             pagesize=letter
                             )
     styles = getSampleStyleSheet()
+    styles.add(ParagraphStyle(name='Centered', alignment=TA_CENTER))
     print(styles)
 
     flowables = []
 
     text = "<para align=center>Hello, I'm a Paragraph</para>"
-    para = Paragraph(text, style=styles["Normal"])
+    para = Paragraph(text, style=styles["Centered"])
     flowables.append(para)
 
     doc.build(flowables)
