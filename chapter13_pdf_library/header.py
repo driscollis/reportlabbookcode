5bcf1e88f698b343d675c7add21f4fe95f9004d1
Mike Driscoll <mike@pythonlibrary.org>
2018-04-30 14:57:27 -0500

    Added initial files for chapter 13

---
+++ chapter13_pdf_library/header.py
@@ -0,0 +1,36 @@
+# header.py
+
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import Paragraph, Image
+
+
+def header(canvas, doc):
+    width, height = doc.pagesize
+    
+    styles = getSampleStyleSheet()
+    
+    ptext = '<font size=10><b>Statement Date: {}' \
+        '</b></font>'.format('01/01/2017')
+    
+    img = Image(doc.logo_path, width=76.2, height=76.2)
+    img.wrapOn(canvas, width, height)
+    img.drawOn(canvas, 100, 725)
+
+    p = Paragraph(ptext, styles["Normal"])
+    p.wrapOn(canvas, width, height)
+    p.drawOn(canvas, 400, 800)
+
+    ptext = '''<font size=10>
+    <b>Member:</b> {member}<br/>
+    <b>Member ID:</b> {member_id}<br/>
+    <b>Group #:</b> {group_num}<br/>
+    <b>Group name:</b> {group_name}<br/>
+    </font>
+    '''.format(member=doc.data.member_name,
+               member_id=doc.data.member_id,
+               group_num=doc.data.group_num,
+               group_name=doc.data.group_name
+               )
+    p = Paragraph(ptext, styles["Normal"])
+    p.wrapOn(canvas, width, height)
+    p.drawOn(canvas, 400, 730)
---
+++ chapter13_pdf_library/main.py
@@ -0,0 +1,78 @@
+# main.py
+
+import argparse
+import os
+
+from header import header
+from lxml import objectify
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import Paragraph
+from reportlab.platypus import SimpleDocTemplate
+
+def get_args():
+    parser = argparse.ArgumentParser(
+            description="Custom PDF Header with Logos")
+    parser.add_argument('-l', '-logo', '--logo', 
+                            action='store',
+                            required=True,
+                            help="The logo's file path",
+                            dest='logo')
+    parser.add_argument('-d', '--data_file', 
+                            action='store',
+                            required=True,
+                            help="The data file path",
+                            dest='data_file')
+    parser.add_argument('-f', '-filepath', '--filepath',
+                            action='store',
+                            required=True,
+                            help="The file path",
+                            dest='path')
+    arguments = parser.parse_args()
+
+    return arguments
+
+
+def parse_xml(xml_file):
+    with open(xml_file) as f:
+        xml = f.read()
+        
+    root = objectify.fromstring(xml)
+    return root
+
+
+def main():
+    arguments = get_args()
+    supported_ext_types = ['.json', '.xml']
+    
+    # Get the file extension
+    _, ext = os.path.splitext(arguments.path)
+    
+    if ext not in supported_ext_types:
+        msg = 'PDF Creator only accepts the following file types: ' \
+        '{}. Got {}'
+        raise RuntimeError(msg.format(str(supported_ext_types, ext)))
+    
+    doc = SimpleDocTemplate(
+        arguments.path,
+        rightMargin=72, leftMargin=36,
+        topMargin=125, bottomMargin=18)
+    
+    if ext == '.xml':
+        data = parse_xml(arguments.data_file)
+    elif ext == '.json':
+        data = parse_json(arguments.data_file)
+    
+    elements = []
+    
+    txt = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
+    styles = getSampleStyleSheet()
+    paragraph = Paragraph(txt, styles["Normal"])
+    elements.append(paragraph)
+    
+    doc.data = xml
+    doc.logo_path = arguments.logo
+
+    doc.build(elements, onFirstPage=header)
+    
+if __name__ == '__main__':
+    main()
