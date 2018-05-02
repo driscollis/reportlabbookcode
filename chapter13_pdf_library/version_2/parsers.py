8f8099efa2017b165863fdad16fa79988380c09f
Mike Driscoll <mike@pythonlibrary.org>
2018-05-02 15:16:25 -0500

    Updated parser

--- chapter13_pdf_library/version_2/parsers.py
+++ chapter13_pdf_library/version_2/parsers.py
@@ -4,15 +4,66 @@ import json
 
 from lxml import objectify
 
+class String(str):
+    """
+    Custom string sub-class to emulate the "strings"
+    returned from lxml
+    """
+    
+    @property
+    def text(self):
+        """
+        Return the str
+        """
+        return self
 
 class Claim:
+    """
+    Represents the data that makes up a claim
+    """
+    
+    def __init__(self, data):
+        self.amount_billed = String(data['amount_billed'])
+        self.amount_remaining = String(data['amount_remaining'])
+        self.coinsurance = String(data['coinsurance'])
+        self.copay = String(data['copay'])
+        self.deductible = String(data['deductible'])
+        self.description = String(data['description'])
+        self.member_rate = String(data['member_rate'])
+        self.pending = String(data['pending'])
+        self.plan_pays = String(data['plan_pays'])
+        self.received_date = String(data['received_date'])
+        self.total_owed = String(data['total_owed'])
+
+
+class Claims:
+    """
+    Represents a series of Claim objects
+    """
     
     def __init__(self, data):
         """
         Accepts a data dictionary and turns it into a
         claim object
         """
-        pass
+        self.data = data
+    
+    def getchildren(self):
+        """
+        Return all the children claim
+        """
+        data_claims = self.data['patient']['claims']['claim']
+        claims = []
+        if isinstance(data_claims, dict):
+            # only a single claim exists
+            claim = Claim(data_claims) 
+            claims.append(claim)
+        else:
+            # claims is a list
+            for claim in data_claims:
+                claims.append(Claim(claim))
+        return claims
+    
 
 class JSON:
     """
@@ -25,17 +76,15 @@ class JSON:
         self.member_id = data['patient']['member_id']
         self.group_num = data['patient']['group_num']
         self.group_name = data['patient']['group_name']
+        self.claim_id = data['patient']['claim_id']
+        self.date = data['patient']['date']
+        self.received_date = data['patient']['received_date']
+        self.sent_to = data['patient']['sent_to']
+        self.amount_owed = data['patient']['amount_owed']
+        self.amount_paid = data['patient']['amount_paid']
         
-        self.claims = []
-        data_claims = ['patient']['claims']['claim']
-        if isinstance(data_claims, dict):
-            # only a single claim exists
-            claim = Claim(data_claims) 
-            self.claims.append(claim)
-        else:
-            # claims is a list
-            for claim in data_claims:
-                self.claims.append(Claim(claim))
+        self.claims = Claims(data)
+        
     
     
 def parse_json(json_file):
